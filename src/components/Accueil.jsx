61c28fe043c4d2830a41eec8f5e88a47a2c77536
unknown872 <80899102+unknown872@users.noreply.github.com>
2025-04-06 20:00:45 +0000

    Update Accueil.jsx
    Adding whatsapp link

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -32,8 +32,13 @@ function Accueil() {
                                 </p>
                                 {/* Boutons */}
                                 <div className="mt-6 flex sm:flex-row flex-col gap-4 sm:justify-start justify-center">
-                                    <a href="#contact"  className=" p-2 px-6 text-white font-semibold rounded-full outline outline-2 outline-purple-500 hover:bg-purple-600 transition ease-in-out delay-150">
-                                        {t("accueil.contact")}
+                                    <a
+                                      href="https://wa.me/221770957560"  
+                                      className="p-2 px-6 text-white font-semibold rounded-full outline outline-2 outline-purple-500 hover:bg-purple-600 transition ease-in-out delay-150"
+                                      target="_blank"  // Ouvre le lien dans un nouvel onglet
+                                      rel="noopener noreferrer"  // Sécurise le lien
+                                    >
+                                      {t("accueil.contact")}
                                     </a>
                                     <a href="/cv-tra.pdf" download className="p-2 px-6 text-white font-semibold rounded-full outline outline-2 outline-pink-500 hover:bg-pink-600 transition ease-in-out delay-150">
                                         {t("accueil.download")}
