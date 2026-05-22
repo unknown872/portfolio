834052faa20f9f020db12cb6a6c3f8cc271c4f74
unknown872 <paulpogboom87@gmail.com>
2026-05-22 23:34:21 +0000

    feat: Adding Quran-app to the portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,6 +7,7 @@ import clsx from "clsx";
 
 // La structure des projets contient une clé i18n pour le titre
 const PROJECTS = [
+    { key: "quran-app", image: "/mockup/quran-app.webp", tech: ["React", "Tailwind CSS", "API Rest", "Mongoose", "MongoDB", "Node.JS", "Express.JS"], link: "https://quran-app-zeta-silk.vercel.app/", category: "Web" },
     { key: "cinetheque", image: "/mockup/cinetheque.webp", tech: ["React", "Tailwind CSS", "Mongoose", "MongoDB", "Node.JS", "Express.JS"], link: "https://cinetheque-lac.vercel.app/", category: "Web" },
     { key: "monvelo", image: "/mockup/monvelo-prfl.webp", tech: ["Next.js", "React", "Tailwind CSS", "Prisma", "Supabase", "Node.JS"], link: "https://monvelo-frontend.vercel.app/", category: "E-Commerce" },
     { key: "senegal24", image: "/mockup/senegal24.webp", tech: ["Next.js", "React", "Tailwind CSS", "Hygraph", "GraphQL"], link: "https://senegal24.vercel.app/", category: "Web" },
